30460ae9ecebd8154d3080e2b10d12589add47e4
sergeishay <sergeishay@gmail.com>
2023-07-31 18:02:58 +0300

    deploy9

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   },
+  images: {
+    domains: ['https://ramon-space.vercel.app/'],
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,9 @@ const App = () => {
     }
   }, [user, router]);
 
-  if (isLoading) return <div className='flex justify-center items-center h-[80vh] w-full text-white text-3xl' >Loading...</div>;
-  if (error) return <div>{error.message}</div>;
 
   return (
+    isLoading ? <div className='flex justify-center items-center h-[80vh] w-full text-white text-3xl' >Loading...</div> :
     <div className='flex flex-col justify-between items-center h-[80vh] w-full'>
       <p className='text-white font-italic text-2xl pt-5' > AI at terrestial speeds</p>
       <Link className='text-blacks font-mid text-3xl bg-whites py-3 px-[80px] rounded-xl shadow-boxtest shadow-whites' href="/api/auth/login">Sign In</Link>
