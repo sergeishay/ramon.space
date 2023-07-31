2d35528a98b32e3690d98e0e50b2502919a4bc34
sergeishay <sergeishay@gmail.com>
2023-07-31 12:30:47 +0300

    deploy3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useRouter } from 'next/router';
+import Link from 'next/link'
 
 const Index = () => {
   const { user, error, isLoading } = useUser();
@@ -18,7 +19,7 @@ const Index = () => {
   return (
     <div className='flex flex-col justify-between items-center h-[80vh] w-full'>
       <p className='text-white font-italic text-2xl pt-5' > AI at terrestial speeds</p>
-      <a className='text-blacks font-mid text-3xl bg-whites py-3 px-[80px] rounded-xl shadow-boxtest shadow-whites' href="/api/auth/login">Sign In</a>
+      <Link className='text-blacks font-mid text-3xl bg-whites py-3 px-[80px] rounded-xl shadow-boxtest shadow-whites' href="/api/auth/login">Sign In</Link>
       <p className='text-transparent font-italic' > AI at terrestial speeds</p>
 
     </div>
