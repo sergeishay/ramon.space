4dacb4d1a441022012adea506e343658f33fb22d
sergeishay <sergeishay@gmail.com>
2023-08-01 12:22:11 +0300

    moobile-works

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -19,37 +19,26 @@ const MainPage = () => {
   const [ModelType, setModelType] = useState("");
   const [files, setFiles] = useState(null);
   const [urlFile, setUrlFile] = useState("");
-  const [isCameraOn, setIsCameraOn] = useState(false);
-  const [videoStream, setVideoStream] = useState(null);
   const [imageSrc, setImageSrc] = useState(null);
-  const videoRef = useRef();
   const router = useRouter();
-  const [theCameraIsOpen, setTheCameraIsOpen] = useState(false); 
   useEffect(() => {
     if (!user) {
       router.push("/");
     }
   }, [user, router]);
 
-  // useEffect(() => {
-  //   if (videoStream && videoRef.current) {
-  //     videoRef.current.srcObject = videoStream;
-  //   }
-  // }, [videoStream]);
+
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  // imageHandler function
-  // const handleInputFile = async (e) => {
-  //   const file = e.target.files[0];
-  //   console.log(file);
-  //   const urlFormat = URL.createObjectURL(file);
-  //   setFiles(file);
-  //   setUrlFile(urlFormat);
-  // };
+
   const handleImageChange = (event) => {
     const file = event.target.files[0];
+    const urlFormat = URL.createObjectURL(file);
+    setFiles(file);
+    setUrlFile(urlFormat);
+
     if (file) {
       const reader = new FileReader();
       reader.onload = (e) => {
@@ -58,50 +47,13 @@ const MainPage = () => {
       reader.readAsDataURL(file);
     }
   };
-  // start Camera function
-  const startCamera = async () => {
-    try {
-      const constraints = {
-        video: {
-          aspectRatio: 16 / 9,
-          width: { ideal: 1280 },
-          height: { ideal: 720 },
-        },
-      };
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      setVideoStream(stream);
-      setIsCameraOn(true);
-    } catch (error) {
-      console.log("Error Accessing Camera:", error);
-    }
-  };
-  const captureImage = () => {
-    const canvas = document.createElement("canvas");
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
 
-    compressImage(canvas, (blob) => {
-      const file = new File([blob], "capturedImage.jpg", {
-        type: "image/jpeg",
-      });
-      handleImageChange({ target: { files: [file] } });
-    });
 
-    stopCamera();
-  };
-  const stopCamera = () => {
-    const tracks = videoRef.current.srcObject.getTracks();
-    tracks.forEach((track) => track.stop());
-    setIsCameraOn(() => false);
-    setVideoStream(() => null);
-  };
 
   // handleImageInputChange function
+
   async function handleImageInputChange() {
     const file = files;
-    console.log(file);
 
     if (file) {
       const maxImageSizeBytes = 20 * 1024 * 1024;
@@ -116,9 +68,8 @@ const MainPage = () => {
       reader.onload = async (e) => {
         const lambda = new AWS.Lambda();
         displayUploadMessage("Uploading image...");
-
         const payload = {
-          name: file.name,
+          name: user.name,
           image: { name: file.name, data: e.target.result },
         };
         const lambdaParams = {
@@ -129,7 +80,6 @@ const MainPage = () => {
         };
         try {
           const lambdaResponse = await lambda.invoke(lambdaParams).promise();
-          console.log(lambdaResponse);
           const responseBody = JSON.parse(lambdaResponse.Payload).body;
           displaySuccessMessage(responseBody);
         } catch (error) {
@@ -181,31 +131,6 @@ const MainPage = () => {
 
       <div className="container">
         <form onSubmit={handleFormSubmit}>
-          {!isCameraOn && (
-            <button type="button" onClick={startCamera}>
-              Open Camera
-            </button>
-          )}
-          {/* {videoStream && (
-            <div className="flex justify-center items-center ">
-              <video
-                ref={(element) => {
-                  videoRef.current = element;
-                }}
-                width="600"
-                height="400"
-                autoPlay
-                className="video-class"
-              ></video>
-
-              <button className="captureTheImage" type="button" onClick={captureImage}>
-                Capture Image
-              </button>
-              <button type="button" className="stopTheImage" onClick={stopCamera}>
-                Close Camera
-              </button>
-            </div>
-          )} */}
          <div >
             <input
               type="file"
@@ -213,22 +138,15 @@ const MainPage = () => {
               capture="environment"
               onChange={handleImageChange}
             />
-            <button className="captureTheImage" type="button" onClick={captureImage}>
-              Capture Image
-            </button>
-            <button type="button" className="stopTheImage" onClick={stopCamera}>
-              Close Camera
-            </button>
-            {imageSrc && <img src={imageSrc} alt="Captured" />}
           </div>
           {/* Image Preview */}
-          {/* {urlFile && (
+          {imageSrc && (
             <div className="flex justify-center items-center flex-col" >
               <h3>Image Preview:</h3>
               <button onClick={removeImage}>X</button>
               <Image src={urlFile} height={400} width={400} alt="Preview" />
             </div>
-          )} */}
+          )}
           <button id="upload-button">Upload Image</button>
         </form>
       </div>
