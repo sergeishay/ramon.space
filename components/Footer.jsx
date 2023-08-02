bd28a82321d79180a9308e2476b05522762a331d
sergeishay <sergeishay@gmail.com>
2023-08-02 15:45:25 +0300

    bug-fix4

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import {footerImg} from '../public/images'
 const Footer = () => {
   return (
-    <div className="footer flex bg-white h-[10vh] justify-center p-0 m-0 align-center fixed bottom-0 left-0 right-0" > 
+    <div className="footer flex bg-white h-[10vh] justify-center pb-4 m-0 align-center " > 
     <Image src={footerImg} width={450} height={100} alt="Ramon.Space Logo" />
 </div>
   )
