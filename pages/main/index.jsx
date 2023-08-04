18cf78dd2171dfd0905bd2d60ebe08b41bc1f196
sergeishay <sergeishay@gmail.com>
2023-08-04 14:24:45 +0300

    final-ver6

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -168,7 +168,7 @@ const MainPage = () => {
   };
 
   return (
-    <div className="flex flex-col justify-between align-center h-[80vh] w-full text-whites text-center">
+    <div className="flex flex-col justify-between align-center h-[100vh] w-full text-whites text-center">
       {showPopup && (
         <div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center bg-black bg-opacity-50">
           <div className="bg-white p-8 rounded-md">
